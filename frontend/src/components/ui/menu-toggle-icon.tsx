0e326ae7143e2450c6cb22e69a3af7d0a6c14a22
Manas Dutta <122201926+manasdutta04@users.noreply.github.com>
2026-05-31 18:47:43 +0530

    feat: add UI primitives including button component, menu toggle icon, and utility helpers

--- frontend/src/components/ui/menu-toggle-icon.tsx
+++ frontend/src/components/ui/menu-toggle-icon.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { cn } from '../../lib/utils';
+import { cn } from '@/lib/utils';
 
 type MenuToggleProps = React.ComponentProps<'svg'> & {
 	open: boolean;
